a26ee85e7209e1e334428f1be17e8a38bac83545
jmattaa <mattajonathan1@gmail.com>
2025-02-16 22:09:06 +0100

    update the shlrfile.sh to work with this project

--- shlrfile.sh
+++ shlrfile.sh
@@ -1,27 +1,36 @@
 cc="gcc"
-cxx="g++"
 
-#targ build
-#depends targ2
-$(cc) src/main.c -o main
-echo "build"
-echo "done"
+build="bin"
+src="src"
+obj="$build/obj"
+
+exec="$build/shlr"
+
+sources=$(find "$src" -name "*.c")
+objects=$(echo "$sources" | sed "s|^$src/|$obj/|; s|\.c$|\.o|")
+
+cflags="-g -Iinclude"
+lflags="-g -ggdb -fsanitize=address"
+
+#targ builddev
+#depends buildObjs
+$cc $lflags -o $exec $objects
 #endtarg
 
-#targ targ2
-echo "pre build"
+#targ buildObjs
+#depends mkdirs
+for source in $sources; do
+  object=$(echo "$source" | sed "s|^$src/|$obj/|; s|\.c$|\.o|")
+  $cc $cflags -c "$source" -o "$object"
+done
 #endtarg
 
-# a normal comment just dosent start with a keyword
-# (targ, depends, endtarg)
+#targ mkdirs
+mkdir -p $build
+mkdir -p $obj
+echo "$sources" | sed "s|^$src|$obj|; s|/[^/]*$||" | sort -u | xargs -I {} mkdir -p {}
+#endtarg
 
 #targ clean
-rm main
-echo "clean"
-echo "done"
-#endtarg
-
-#targ doAll
-#depends build clean
-echo "built and then cleaned hihi"
+rm -rf $build
 #endtarg
